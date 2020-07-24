e39253049044d5197c0f72493acc31e545e12eb1
Gosia Brajtkopf <gosiabrajtkopf@gmail.com>
2020-07-24 13:14:36 +0200

    Updates

---
+++ precache-manifest.d07f070f2bc3a1d90bd97312715d6e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d26d3b32ec9a75d1279c052e1c71892",
+    "url": "/cytaty/index.html"
+  },
+  {
+    "revision": "44d62b735d12c8722819",
+    "url": "/cytaty/static/css/main.7193ab3f.chunk.css"
+  },
+  {
+    "revision": "7a2c2a3bbea9b0073d16",
+    "url": "/cytaty/static/js/2.44acadbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cytaty/static/js/2.44acadbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d62b735d12c8722819",
+    "url": "/cytaty/static/js/main.4b1505fa.chunk.js"
+  },
+  {
+    "revision": "f0c8a078148d2e5b70c2",
+    "url": "/cytaty/static/js/runtime-main.05b3e402.js"
+  }
+]);
