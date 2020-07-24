1be433836da8cff046018eaa3aac2af8a36f75f1
Gosia Brajtkopf <gosiabrajtkopf@gmail.com>
2020-07-24 13:13:27 +0200

    finished the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cytat: {},
+    }
+  }
+  componentDidMount() {
+    fetch('https://cytaty.herokuapp.com/api/quotes/random')
+      .then((response) => response.json())
+      .then((cytaty) => this.setState({ cytat: cytaty[0] }))
+      .catch(console.log)
+  }
+
+  handleClick = () => {
+    fetch('https://cytaty.herokuapp.com/api/quotes/random')
+      .then((response) => response.json())
+      .then((cytaty) => this.setState({ cytat: cytaty[0] }))
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <>
+        <div className='quote-container'>
+          <div className='quote'>
+            <h1>"{this.state.cytat.text}"</h1>
+            <h3>{this.state.cytat.author}</h3>
+          </div>
+          <div className='button-cont'>
+            <button onClick={this.handleClick}>Następny</button>
+          </div>
+        </div>
+
+        <div className='background'></div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
